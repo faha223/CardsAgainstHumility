2d6bfa16a161c551cf46d3284a760dfe397ee9c7
fred.hallock <fred.hallock@PC-FRED>
2016-06-30 12:53:28 -0400

    Added some stuff to convert the card text to inlines for proper rendering. I have yet to see any effective results from this so far.

--- CardsAgainstHumility.WP8/GamePage.xaml.cs
+++ CardsAgainstHumility.WP8/GamePage.xaml.cs
@@ -4,6 +4,10 @@ using CardsAgainstHumility.WP8.ViewModels;
 using Microsoft.Xna.Framework.Input.Touch;
 using System.Windows.Input;
 using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Collections.Generic;
 
 namespace CardsAgainstHumility.WP8
 {
@@ -85,6 +89,44 @@ namespace CardsAgainstHumility.WP8
                 return;
             }
             base.OnBackKeyPress(e);
+        }
+
+        public static string GetInlineList(TextBlock element)
+        {
+            if (element != null)
+                return element.GetValue(ArticleContentProperty) as string;
+            return string.Empty;
+        }
+
+        public static void SetInlineList(TextBlock element, string value)
+        {
+            if (element != null)
+                element.SetValue(ArticleContentProperty, value);
+        }
+
+        public static readonly DependencyProperty ArticleContentProperty =
+            DependencyProperty.RegisterAttached(
+                "InlineList",
+                typeof(List<Inline>),
+                typeof(GamePage),
+                new PropertyMetadata(null, OnInlineListPropertyChanged));
+
+        private static void OnInlineListPropertyChanged(DependencyObject obj,
+            DependencyPropertyChangedEventArgs e)
+        {
+            var tb = obj as TextBlock;
+            if (tb != null)
+            {
+                // clear previous inlines
+                tb.Inlines.Clear();
+
+                // add new inlines
+                var inlines = e.NewValue as List<Inline>;
+                if (inlines != null)
+                {
+                    inlines.ForEach(inl => tb.Inlines.Add((inl)));
+                }
+            }
         }
     }
 }
